f37898b2269ddb3c4dc7eb49748e59caeaef37a1
2DGP Student <2dgp-student@users.noreply.github.com>
2026-09-22 14:44:24 +0900

    Update shape movement placeholders

---
+++ day2.py
@@ -0,0 +1,19 @@
+def move_circle():
+    print('circle')
+    pass
+def move_rectangle():
+    print('rectangle')
+    pass
+def move_triangle():
+    print('triangle')
+    pass
+
+
+
+
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+    pass
