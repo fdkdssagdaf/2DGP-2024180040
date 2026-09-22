5d694c2c87b30a5520a5d46f64753846d48da8a0
2DGP Student <2dgp-student@users.noreply.github.com>
2026-09-22 15:00:51 +0900

    Display character image in movement loop

--- day2.py
+++ day2.py
@@ -1,19 +1,34 @@
+from pico2d import *
+
+open_canvas(800, 600)
+character = load_image('character.png')
+
+
+
+
+
+
+
+
+
+
+
 def move_circle():
     print('circle')
-    pass
+
 def move_rectangle():
     print('rectangle')
-    pass
+
 def move_triangle():
     print('triangle')
-    pass
-
-
-
-
 
 while True:
+    clear_canvas()
     move_circle()
     move_rectangle()
     move_triangle()
-    pass
+    character.draw(400, 300)
+    update_canvas()
+    delay(0.03)
+
+close_canvas()
