cb8d2f5396b09d32f33755b82ecdd6541f59f34d
fdkdssagdaf <vngkgk256@gmail.com>
2026-09-22 14:37:33 +0900

    Add placeholder functions for shape movements

---
+++ changes/day2.py
@@ -0,0 +1,15 @@
+def move_circle():
+    pass
+def move_rectangle():
+    pass
+def move_triangle():
+    pass
+
+
+
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+    pass  
